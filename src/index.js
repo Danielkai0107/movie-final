6d9868618d56401d557ccdc14cb9dd3325c62984
Daniel <danielwzk0107@gmail.com>
2022-09-08 00:33:55 +0800

    second

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <MovieProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/movie-final">
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
